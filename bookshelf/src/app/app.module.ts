35456d147d3043d3ef3ddad4341cf67e8111c89d
Rinat Valiullov <4rontender@gmail.com>
2022-08-01 20:16:37 +0500

    refactor(material.module): add all components and change a shared module name

--- bookshelf/src/app/app.module.ts
+++ bookshelf/src/app/app.module.ts
@@ -4,8 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { BookDataComponent } from './book-data/book-data.component';
-import { SharedModule } from './shared/shared.module';
 import { BookShelfComponent } from './book-shelf/book-shelf.component';
+import { MaterialModule } from './shared/material.module';
 
 @NgModule({
   declarations: [
@@ -16,7 +16,7 @@ import { BookShelfComponent } from './book-shelf/book-shelf.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    SharedModule
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
